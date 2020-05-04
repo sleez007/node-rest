6a2c8305dcdf067b902b42183a5649f5a0976d8a
Etoka kingsley <etokakingsley@gmail.com>
2020-05-04 07:10:39 +0100

    MERN STACK WEB APP

---
+++ README.md
@@ -0,0 +1,11 @@
+# FULL MERN STACK APPLICATION
+
+A fully functional crud application built with React, Express, Mongoose. 
+
+> The frontend is a single page application built with react, while the backend is built as a restful service using node Express server
+
+## TODO : Covert the client to use next.js for server side rendering
+
+Contributions are welcomed though this is a dummy project
+
+> NOTE: Ensure you have mongodb setup locally on your machine else you can change the connection string to use any mongodb server.
---
+++ backend/app.js
@@ -0,0 +1,60 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const multer = require('multer');
+const cors = require('cors')
+
+const feedRoutes = require('./routes/feed');
+const authRoutes = require('./routes/auth');
+
+const app = express();
+
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().toISOString() + '-' + file.originalname);
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+app.use(bodyParser.json()); // application/json
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
+);
+app.use(cors());
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use('/feed', feedRoutes);
+app.use('/auth', authRoutes);
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({ message: message, data: data });
+});
+
+mongoose
+  .connect(
+    'mongodb://localhost/blog'
+  )
+  .then(result => {
+    app.listen(8080);
+  })
+  .catch(err => console.log(err));
